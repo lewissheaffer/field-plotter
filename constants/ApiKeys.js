1dbe01ea71f5a518dd390788607f4256ab2ffe53
lewissheaffer <lewiss@iastate.edu>
2020-05-14 20:21:58 -0500

    Test commit for branching and git ignore

--- constants/ApiKeys.js
+++ constants/ApiKeys.js
@@ -1,12 +1,3 @@
 export default {
-  /*firebaseConfig = {
-    apiKey: "AIzaSyA0-RKMUYu2aRwbp4lT9ftCAzKwJU8ESXQ",
-    authDomain: "field-plotter.firebaseapp.com",
-    databaseURL: "https://field-plotter.firebaseio.com",
-    projectId: "field-plotter",
-    storageBucket: "field-plotter.appspot.com",
-    messagingSenderId: "249631959104",
-    appId: "1:249631959104:web:2df38c306cf1eb39b317a5",
-    measurementId: "G-NTLFCPW9YN"
-  },*/
+
 }
